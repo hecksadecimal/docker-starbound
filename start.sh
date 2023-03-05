6cbf61fb48e6172deae819037795dd08d59dc283
Hex <hecksadecimal@outlook.com>
2023-03-05 14:49:41 -0500

    Disable crossbound

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 if [ -f /starbound/linux/starbound_server ]; then
-  cd /starbound/discord
-  npm install
-  npm start
+  #cd /starbound/discord
+  #npm install
+  #npm start
+  cd /starbound/linux
+  ./starbound_server
 fi
 
 while [ -f /.update ]; do

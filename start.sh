813e2520babe7232e26c7113f7d3c8259ada1d99
Hex <hecksadecimal@outlook.com>
2023-03-11 20:44:20 -0500

    Enable discord integration again

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if [ -f /starbound/linux/starbound_server ]; then
-  #cd /starbound/discord
-  #npm install
-  #npm start
-  cd /starbound/linux
-  ./starbound_server
+  cd /starbound/discord
+  npm install
+  npm start
+
+  #./starbound_server
 fi
 
 while [ -f /.update ]; do
